43a7369a392c0a3391e56fab234ea8109310be45
ThanhHip85 <nguyenhiep0456@gmail.com>
2024-04-20 16:34:53 +0700

    add roomcard, roomListing component

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,25 @@ import ExistingRooms from './components/room/ExistingRooms'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from './components/home/Home'
 import EditRoom from './components/room/EditRoom'
+import NavBar from './components/layout/NavBar'
+import Footer from './components/layout/Footer'
+import RoomListing from './components/room/RoomListing'
 
 function App() {
   return (
     <>
       <main>
         <Router>
+          <NavBar />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/edit-room/:roomId" element={<EditRoom />} />
             <Route path="/existing-rooms" element={<ExistingRooms />} />
-            <Route path="/add-rooms" element={<AddRoom />} />
-
+            <Route path="/add-room" element={<AddRoom />} />
+            <Route path="/browse-all-rooms" element={<RoomListing />} />
           </Routes>
         </Router>
-
-
+        <Footer />
       </main>
     </>
   )
