d87627a5987f3d486bf8e27170eef0f31e6cf8e0
ThanhHip85 <nguyenhiep0456@gmail.com>
2024-04-21 23:31:41 +0700

    Add component carousel

--- src/components/room/RoomCard.jsx
+++ src/components/room/RoomCard.jsx
@@ -9,7 +9,7 @@ const RoomCard = ({ room }) => {
                     <div className='flex-shrink-0 mr-3 mb-3 mb-md-0' >
                         <Card.Img
                             variant='top'
-                            src={`data:image/png;base64, ${room.photo}`}
+                            src={room.photo ? `data:image/png;base64, ${room.photo}` : 'placeholder_image_url'}
                             alt='Room Photo'
                             style={{ width: "100%", maxWidth: "200px", height: "auto" }} />
                     </div>
