48e21d86e6c5b718f4bab46732a8172d4250c204
ThanhHip85 <nguyenhiep0456@gmail.com>
2024-04-25 21:16:42 +0700

    Add component function find booking

--- src/components/admin/Admin.jsx
+++ src/components/admin/Admin.jsx
@@ -6,7 +6,7 @@ const Admin = () => {
     <section className="container mt-5">
       <h2>Welcome to Admin Panel</h2>
       <hr />
-      <Link to={"/existing-rooms"}>Manage Rooms</Link>
+      <Link to={"/existing-rooms"}>Manage Rooms</Link> <br /> <hr />
       <Link to={"/existing-bookings"}>Manage Bookings</Link>
     </section>
   );
