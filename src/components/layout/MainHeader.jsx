75d516b2151e9da2e1267e09d3ffa2c7fa156bd9
ThanhHip85 <nguyenhiep0456@gmail.com>
2024-05-04 10:58:19 +0700

    Change name header

--- src/components/layout/MainHeader.jsx
+++ src/components/layout/MainHeader.jsx
@@ -1,17 +1,15 @@
-
-
 const MainHeader = () => {
-    return (
-        <header className="header-banner">
-            <div className="overlay"></div>
-            <div className="animated-texts overlay-content">
-                <h1>
-                    Welcome to <span className="hotel-color"> lakeSide Hotel</span>
-                </h1>
-                <h4>Experience the Best Hospitality in Town</h4>
-            </div>
-        </header>
-    )
-}
+  return (
+    <header className="header-banner">
+      <div className="overlay"></div>
+      <div className="animated-texts overlay-content">
+        <h1>
+          Welcome to <span className="hotel-color"> Bình Minh Hotel</span>
+        </h1>
+        <h4>Experience the Best Hospitality in Town</h4>
+      </div>
+    </header>
+  );
+};
 
-export default MainHeader
+export default MainHeader;
